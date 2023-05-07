03e706dce3f21b86460354182161de243041b34a
surumeneco <surumeneko164@gmail.com>
2023-05-07 18:27:29 +0900

    フォルダ構造作成

---
+++ loading/scene_loading.js
@@ -0,0 +1,5 @@
+document.write('<script src="./scene/Loading_scene.js"></script>');
+document.write('<script src="./scene/Title_scene.js"></script>');
+document.write('<script src="./scene/Main_scene.js"></script>');
+document.write('<script src="./scene/Gameover_scene.js"></script>');
+
